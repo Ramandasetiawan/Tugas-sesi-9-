cfdf82e7714ac05eb5bc8a52fe6c4548433e8cba
Ramandasetiawan <160981847+Ramandasetiawan@users.noreply.github.com>
2024-05-10 08:31:07 +0700

    Add files via upload

---
+++ DASPROG Ramanda Sesi9 (TUGAS).py
@@ -0,0 +1,63 @@
+# NO.1
+# angka = [8,3,12,4,7,2]
+
+# angka = [num if num >= 5 else 0 for num in angka ]
+# angka.sort(reverse = True)
+
+# print(angka)
+
+
+
+
+
+# NO.2
+# angka = [7, 4, 9, 2, 5, 1]
+
+# filtered_angka = [num for num in angka if num % 2 == 0]
+# filtered_angka.sort()
+
+# print(filtered_angka)
+
+
+
+
+# NO.3
+# buah = ["apel","jeruk","mangga","pisang","angggur","durian"]
+
+# buah = list(filter(lambda y: len(y) >= 5, buah ))
+# buah = sorted (buah)
+
+# print(buah)
+
+
+
+
+
+# NO.4
+# list1 = ["apel","jeruk","mangga"]
+# list2 = ["apel","anggur","nanas"]
+
+# gabung = list(set(list1 + list2))
+# hasil = sorted(gabung)
+
+# print(hasil)
+
+
+
+
+# # NO.5
+# number = [105,20,8,150,30,5]
+
+# nilai = list(filter(lambda y:y  >= 10 and y <= 100, number ))
+# nilai = sorted(nilai)
+
+# print(nilai)
+
+
+
+
+
+
+
+
+
